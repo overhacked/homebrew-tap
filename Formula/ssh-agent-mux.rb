50e821cf9256ce6d26cc706498c72fb9608d617b
homebrew-releaser@example.com <homebrew-releaser@example.com>
2025-04-28 23:41:34 +0000

    chore: brew formula update for ssh-agent-mux v0.1.6

--- Formula/ssh-agent-mux.rb
+++ Formula/ssh-agent-mux.rb
@@ -5,32 +5,32 @@
 class SshAgentMux < Formula
   desc "Combines multiple agents' keys into a single agent"
   homepage "https://github.com/overhacked/ssh-agent-mux"
-  url "https://github.com/overhacked/ssh-agent-mux/archive/refs/tags/v0.1.5.tar.gz"
-  version "0.1.5"
-  sha256 "55cc828c65b7ae1d68a1c77f03a1959a410508a613636b93284b77a228957068"
+  url "https://github.com/overhacked/ssh-agent-mux/archive/refs/tags/v0.1.6.tar.gz"
+  version "0.1.6"
+  sha256 "071decb6d9474dc7bed4f190af3b01db6c9ffc42ec9a26c537ecb98fde6e8d4b"
   license "BSD-3-Clause"
 
   on_macos do
     on_intel do
-      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.5/ssh-agent-mux-0.1.5-darwin-amd64.tar.gz"
-      sha256 "3ac6239dd081b9f232a1e82b95c93941704f5287f85cee80ef29f37a4372e000"
+      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.6/ssh-agent-mux-0.1.6-darwin-amd64.tar.gz"
+      sha256 "e7130935576b24d1649740855c13cb2bb9a186deeb7d962febe4d835a367197b"
     end
 
     on_arm do
-      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.5/ssh-agent-mux-0.1.5-darwin-arm64.tar.gz"
-      sha256 "8727a8f0903a32356b83b6718f31e18fe07b83393cf54e70d09e888b4b90b1ad"
+      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.6/ssh-agent-mux-0.1.6-darwin-arm64.tar.gz"
+      sha256 "10935d3f0f01e23291111698345b05eda76cf8f4662164e3e3eccaa49c0f1431"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.5/ssh-agent-mux-0.1.5-linux-amd64.tar.gz"
-      sha256 "009a185af10fc45f0c8a297b57a2728d4ac47efed8d5611731e8ee1c8da49172"
+      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.6/ssh-agent-mux-0.1.6-linux-amd64.tar.gz"
+      sha256 "bc3f3d8296aa68e5eb11568f67b597b7839aa29295fa8d2f96ce9a76371a38da"
     end
 
     on_arm do
-      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.5/ssh-agent-mux-0.1.5-linux-arm64.tar.gz"
-      sha256 "a850493d4c60ca8782fd66ea5674f6e41efb682f90ed351bba6225f46799075c"
+      url "https://github.com/overhacked/ssh-agent-mux/releases/download/v0.1.6/ssh-agent-mux-0.1.6-linux-arm64.tar.gz"
+      sha256 "da1807a6177e174d6dc10227b1a3c963bee946706cff951bd4b85dcabb1f24ff"
     end
   end
 
@@ -39,11 +39,10 @@ class SshAgentMux < Formula
   end
 
   test do
+    test_sock = testpath/"#{name}.sock"
     agent_stdout = shell_output(
-        %Q(sock="#{testpath}/ssh-agent-mux.sock";) \
-        + %Q( ssh-agent-mux --log-level info --config /dev/null --listen "$sock" &) \
-        + %Q( until [ -e "$sock" ]; do sleep 0.1; done;) \
-        + %Q( kill %1; wait %1)
+      %Q(#{opt_bin/name} --log-level info --config /dev/null --listen "#{test_sock}" &) \
+      + %Q( until [ -e "#{test_sock}" ]; do sleep 0.1; done; kill %1; wait %1),
     )
     assert_match "Starting agent", agent_stdout
     assert_match "Exiting on SIGTERM", agent_stdout
